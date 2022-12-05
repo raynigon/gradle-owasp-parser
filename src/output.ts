5597eb5ffcd57f377427da02ea5ab12abb6046da
Simon Schneider <10846939+raynigon@users.noreply.github.com>
2022-12-05 11:18:54 +0100

    Add Markdown Report

--- src/output.ts
+++ src/output.ts
@@ -1,5 +1,15 @@
-import { OwaspReport, Vulnerability, VulnerableDependency } from "./parser";
+import { OwaspReport, VulnerableDependency } from "./parser";
 import * as core from '@actions/core';
+
+function generateMarkdownReport(report: OwaspReport): string {
+    const header = "| Dependency | Vulnerabilities |\n" +
+                   "| ---------- | --------------- |\n";
+    let body = ""
+    for(let dependency of report.dependencies){
+        body += `| ${dependency.coordinates.groupId}:${dependency.coordinates.artifactId} | ${Array.from(dependency.vulnerabilities).join(' ')} |\n`
+    }
+    return header + body
+}
 
 export async function writeOutput(report: OwaspReport) {
     const vulnerabilities = new Set(report.dependencies
@@ -8,4 +18,5 @@ export async function writeOutput(report: OwaspReport) {
     core.setOutput("vulnerability_count", report.dependencies.length)
     core.setOutput("vulnerable_dependencies", vulnerabilities.size)
     core.setOutput("vulnerabilities", Array.from(vulnerabilities).join(' '))
+    core.setOutput("report_markdown", generateMarkdownReport(report))
 }
