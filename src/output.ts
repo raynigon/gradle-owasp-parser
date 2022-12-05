b01c87f4731e0134e69b9f2a8b53c530587f9ac8
Simon Schneider <10846939+raynigon@users.noreply.github.com>
2022-12-05 12:43:29 +0100

    Update Markdown Report

--- src/output.ts
+++ src/output.ts
@@ -3,18 +3,21 @@ import * as core from '@actions/core';
 
 function generateMarkdownReport(report: OwaspReport): string {
     const prefix = "The following dependencies contain vulnerabilities. Please check if these vulnerabilities are relevant and ignore them if necessary.\n\n"
-    const header = "| Vulnerability | Dependency | Description | IgnoreXML |\n" +
-                   "| ------------- | ---------- | ----------- | --------- |\n";
-    let body = ""
+    const header = "<thead>\n<th>Vulnerability</th>\n<th>Dependency</th>\n<th>Description</th>\n<th>IgnoreXML</th>\n</thead>\n"
+    let body = "<tbody>\n"
     for(let dependency of report.dependencies){
         for(let vulnerability of dependency.vulnerabilities) {
-            let description = vulnerability.description.replace("\n", " ")
-            body += `| ${vulnerability.name} | ${dependency.coordinates.groupId}:${dependency.coordinates.artifactId} `+
-                    `| ${description} | \`\`\`<suppress><cve>${vulnerability.name}</cve></suppress>\`\`\` |\n`
-
+            const description = vulnerability.description.replace("\n", "<br />")
+            body += "<tr>"
+            body += `<td>${vulnerability.name}</td>`
+            body += `<td>${dependency.coordinates.groupId}:${dependency.coordinates.artifactId}</td>`
+            body += `<td>${description}</td>`
+            body += `<td><pre lang="xml">&lt;suppress&gt;&lt;cve>${vulnerability.name}&lt;/cve&gt;&lt;/suppress&gt;</pre></td>`
+            body += "</tr>"
         }        
     }
-    return prefix + header + body
+    body += "</tbody>"
+    return prefix + "<table>" + header + body + "</table>"
 }
 
 export async function writeOutput(report: OwaspReport) {
@@ -26,3 +29,16 @@ export async function writeOutput(report: OwaspReport) {
     core.setOutput("vulnerabilities", Array.from(vulnerabilities).join(' '))
     core.setOutput("report_markdown", generateMarkdownReport(report))
 }
+
+/*
+The following dependencies contain vulnerabilities. Please check if these vulnerabilities are relevant and ignore them if necessary.
+<table>
+<tr>
+<td>CVE-2022-41946</td>
+<td>org.postgresql:postgresql</td>
+<td>pgjdbc is an open source postgresql JDBC Driver. In affected versions a prepared statement using eit</td>
+<td><pre lang="xml">&lt;suppress&gt;&lt;cve>CVE-2022-41946&lt;/cve&gt;&lt;/suppress&gt;</pre></td>
+</tr>
+</table>
+<!-- Comment-ID: owasp-report-refs/pull/656/merge -->
+*/
